865c3eaa660d7580df8d825492b7d4f481dea006
Nuoxxx <yesiwillbetherexxx@gmail.com>
2017-08-16 18:13:21 +0800

    获取最新的测试报告

--- Appium_HtmlTestRunner/test_mail/newfile.py
+++ Appium_HtmlTestRunner/test_mail/newfile.py
@@ -1,8 +1,12 @@
 # coding = utf-8
+"""
+获取最新的测试报告
+"""
 import os,datetime,time
 
-result_dir = 'D:\\Selenium_project\\report'
+# result_dir = 'D:\\Selenium_project\\report'
 
+result_dir = os.path.abspath(os.path.join(os.path.abspath(__file__),"../../report"))
 lists = os.listdir(result_dir)
 #fn就是lists中的元素，自动遍历
 lists.sort(key = lambda fn: os.path.getmtime(result_dir+"\\"+fn)
